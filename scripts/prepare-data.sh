70c5cfb8cb863b5a6600c60c754e4e67ede8de47
Otavio Rodolfo Piske <angusyoung@gmail.com>
2024-11-18 16:50:24 +0100

    Added conversion scripts

---
+++ scripts/prepare-data.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#CODE_DIR=${1:-"$HOME/code/java/camel"}
+
+install_path=$(dirname "$0")
+CODE_DIR=$(echo "$1" | jq -r .path)
+
+logDir=$(mktemp -d)
+
+# Step 1: convert all component documentation to a docbook file
+find "$CODE_DIR" -type f -iname '*.adoc' -ipath '*src/main/docs*' -exec "${install_path}"/convert-file.sh {} "$logDir" \; | tee $logDir/conversion.log
+
+numDocs=$(find "$CODE_DIR" -type f -iname '*.adoc' -ipath '*src/main/docs*' | wc -l)
+numConverted=$(find "$CODE_DIR" -type f -iname '*.xml' -ipath '*src/main/docs*' | wc -l)
+printf "Conversion complete. Checking results: converted %s of %s\n" "$numConverted" "$numDocs"
+printf "Check %s for results and failures" "$logDir/conversion.log"
