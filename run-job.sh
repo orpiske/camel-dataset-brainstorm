4f7d28d1f23e04616cfab17a91d4d862be0c20c2
Otavio Rodolfo Piske <angusyoung@gmail.com>
2024-12-06 18:06:55 +0100

    Adjust to upload the route

--- run-job.sh
+++ run-job.sh
@@ -2,4 +2,4 @@
 
 SERVICE_HOST=${1:-"localhost:8080"}
 
-cat pipeline.yaml| base64 | curl -X POST -H "Content-Type: application/json"  --verbose --data-binary @- ${SERVICE_HOST}/api/v1/pipeline/yaml
+cat yaml/routes.yaml| base64 | curl -X POST -H "Content-Type: application/json"  --verbose --data-binary @- ${SERVICE_HOST}/api/v1/acquisition/service
