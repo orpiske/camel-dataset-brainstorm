4a7f891b6b6c4c85b7a069f879adea72d0405ac9
Otavio Rodolfo Piske <angusyoung@gmail.com>
2025-01-24 18:44:57 +0100

    Adjusted to use the variables set by the operator

--- transformation/02/transform.sh
+++ transformation/02/transform.sh
@@ -4,17 +4,15 @@ install_path=$(dirname "$0")
 VERSION=4.8.0
 
 JAR_FILE=${install_path}/camel-jbang-plugin-explain-${VERSION}-jar-with-dependencies.jar
-BRAINSTORM_HOME=/opt/brainstorm/
-BASE_DATA_DIR=${BRAINSTORM_HOME}/data
-DATA_DIR=${BASE_DATA_DIR}/work/camel/
+SOURCE_DIR=${DATA_DIR}/camel/
 
 echo "Generating the dataset"
-java -jar "${JAR_FILE}" data dump --data-type component-documentation --source-path ${DATA_DIR}
+java -jar "${JAR_FILE}" data dump --data-type component-documentation --source-path "${SOURCE_DIR}"
 
-if [[ -d ${BASE_DATA_DIR}/dataset ]] ; then
+if [[ -d ${DATA_DIR}/dataset ]] ; then
   echo "Removing old dataset"
-  rm -rf ${BASE_DATA_DIR}/dataset
+  rm -rf "${DATA_DIR}"/dataset
 fi
 
 echo "Moving the dataset"
-mv dataset ${BASE_DATA_DIR}
+mv dataset "${DATA_DIR}"
